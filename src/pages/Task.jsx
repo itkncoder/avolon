788b50627ac20c4f4e0fd29befc71cf9d0b0a6b7
KNCoder <itkncoder@gmail.com>
2023-02-02 00:16:58 +0500

    perfelying viptasks & task

--- src/pages/Task.jsx
+++ src/pages/Task.jsx
@@ -1,47 +1,59 @@
 import logo from "../assets/logo.jpg"
+import {Link} from "react-router-dom"
 
 const Task = () => {
     return (  
-        <div className="min-h-screen flex justify-center items-center">
-            <div className="md:max-w-screen-md w-full">
+        <div className="min-h-screen flex justify-center items-start">
+            <Link to="/viptasks">
+                <i className="hover:bg-zinc-800 transition-all fa-solid fa-angle-left absolute top-2 left-2 w-8 rounded-full h-8 flex justify-center items-center bg-zinc-900 text-yellow-500"></i>
+            </Link>
+            <div className="md:max-w-screen-md w-full mt-14">
                 <div className="fon p-3 m-2 rounded-xl flex justify-between items-center">
                     <div className="flex flex-col items-start">
-                        <h2 className="italic font-semibold text-2xl text-white">VIP1</h2>
-                        <p className="text-white">Mavjud vazifalar soni <span className="text-rose-500">1</span></p>
+                        <h2 className="italic font-semibold text-4xl text-white">VIP1</h2>
+                        <p className="text-white text-lg font-semibold">Mavjud vazifalar soni <span className="text-rose-500">1</span></p>
                     </div>
                     <img className="w-14 rounded-full" src={logo} alt="logo" />
                 </div>
-                <div>
+                <div className="max-h-96 overflow-y-auto">
                     <div className="fon p-3 m-2 rounded-xl flex justify-between items-center gap-1">
                         <img className="w-10 sm:w-14" src={logo} alt="product" />
-                        <p className="text-xs text-white font-semibold w-5/12 sm:text-sm sm:w-fit">Sahroda elektr energiyasi ishlab chiqarish </p>
-                        <div className="flex flex-col items-center w-4/12 sm:w-fit">
-                            <button className="text-3xs w-full hover:bg-gray-900 cursor-pointer transition-all bg-zinc-900 py-1 px-1 rounded-3xl text-yellow-400 font-semibold sm:text-sm sm:w-fit sm:px-4 sm:py-2">Qurilmani oling</button>
-                            <p className="text-sm font-semibold">7000 UZS</p>
+                        <p className="text-base text-white font-semibold w-5/12 sm:text-lg sm:w-fit">Sahroda elektr energiyasi ishlab chiqarish </p>
+                        <div className="flex flex-col items-center sm:w-fit">
+                            <button className="text-sm w-full hover:bg-gray-900 cursor-pointer transition-all bg-zinc-900 py-1 px-2 rounded-3xl text-yellow-400 font-semibold sm:text-sm sm:w-fit sm:px-4 sm:py-2">Qurilmani oling</button>
+                            <p className="font-semibold text-base">7000 UZS</p>
                         </div>
                     </div>
                     <div className="fon p-3 m-2 rounded-xl flex justify-between items-center gap-1">
                         <img className="w-10 sm:w-14" src={logo} alt="product" />
-                        <p className="text-xs text-white font-semibold w-5/12 sm:text-sm sm:w-fit">Sahroda elektr energiyasi ishlab chiqarish </p>
-                        <div className="flex flex-col items-center w-4/12 sm:w-fit">
-                            <button className="text-3xs w-full hover:bg-gray-900 cursor-pointer transition-all bg-zinc-900 py-1 px-1 rounded-3xl text-yellow-400 font-semibold sm:text-sm sm:w-fit sm:px-4 sm:py-2">Qurilmani oling</button>
-                            <p className="text-sm font-semibold">7000 UZS</p>
+                        <p className="text-base text-white font-semibold w-5/12 sm:text-lg sm:w-fit">Sahroda elektr energiyasi ishlab chiqarish </p>
+                        <div className="flex flex-col items-center sm:w-fit">
+                            <button className="text-sm w-full hover:bg-gray-900 cursor-pointer transition-all bg-zinc-900 py-1 px-2 rounded-3xl text-yellow-400 font-semibold sm:text-sm sm:w-fit sm:px-4 sm:py-2">Qurilmani oling</button>
+                            <p className="font-semibold text-base">7000 UZS</p>
                         </div>
                     </div>
                     <div className="fon p-3 m-2 rounded-xl flex justify-between items-center gap-1">
                         <img className="w-10 sm:w-14" src={logo} alt="product" />
-                        <p className="text-xs text-white font-semibold w-5/12 sm:text-sm sm:w-fit">Sahroda elektr energiyasi ishlab chiqarish </p>
-                        <div className="flex flex-col items-center w-4/12 sm:w-fit">
-                            <button className="text-3xs w-full hover:bg-gray-900 cursor-pointer transition-all bg-zinc-900 py-1 px-1 rounded-3xl text-yellow-400 font-semibold sm:text-sm sm:w-fit sm:px-4 sm:py-2">Qurilmani oling</button>
-                            <p className="text-sm font-semibold">7000 UZS</p>
+                        <p className="text-base text-white font-semibold w-5/12 sm:text-lg sm:w-fit">Sahroda elektr energiyasi ishlab chiqarish </p>
+                        <div className="flex flex-col items-center sm:w-fit">
+                            <button className="text-sm w-full hover:bg-gray-900 cursor-pointer transition-all bg-zinc-900 py-1 px-2 rounded-3xl text-yellow-400 font-semibold sm:text-sm sm:w-fit sm:px-4 sm:py-2">Qurilmani oling</button>
+                            <p className="font-semibold text-base">7000 UZS</p>
                         </div>
                     </div>
                     <div className="fon p-3 m-2 rounded-xl flex justify-between items-center gap-1">
                         <img className="w-10 sm:w-14" src={logo} alt="product" />
-                        <p className="text-xs text-white font-semibold w-5/12 sm:text-sm sm:w-fit">Sahroda elektr energiyasi ishlab chiqarish </p>
-                        <div className="flex flex-col items-center w-4/12 sm:w-fit">
-                            <button className="text-3xs w-full hover:bg-gray-900 cursor-pointer transition-all bg-zinc-900 py-1 px-1 rounded-3xl text-yellow-400 font-semibold sm:text-sm sm:w-fit sm:px-4 sm:py-2">Qurilmani oling</button>
-                            <p className="text-sm font-semibold">7000 UZS</p>
+                        <p className="text-base text-white font-semibold w-5/12 sm:text-lg sm:w-fit">Sahroda elektr energiyasi ishlab chiqarish </p>
+                        <div className="flex flex-col items-center sm:w-fit">
+                            <button className="text-sm w-full hover:bg-gray-900 cursor-pointer transition-all bg-zinc-900 py-1 px-2 rounded-3xl text-yellow-400 font-semibold sm:text-sm sm:w-fit sm:px-4 sm:py-2">Qurilmani oling</button>
+                            <p className="font-semibold text-base">7000 UZS</p>
+                        </div>
+                    </div>
+                    <div className="fon p-3 m-2 rounded-xl flex justify-between items-center gap-1">
+                        <img className="w-10 sm:w-14" src={logo} alt="product" />
+                        <p className="text-base text-white font-semibold w-5/12 sm:text-lg sm:w-fit">Sahroda elektr energiyasi ishlab chiqarish </p>
+                        <div className="flex flex-col items-center sm:w-fit">
+                            <button className="text-sm w-full hover:bg-gray-900 cursor-pointer transition-all bg-zinc-900 py-1 px-2 rounded-3xl text-yellow-400 font-semibold sm:text-sm sm:w-fit sm:px-4 sm:py-2">Qurilmani oling</button>
+                            <p className="font-semibold text-base">7000 UZS</p>
                         </div>
                     </div>
                 </div>
--- src/pages/VipTasks.jsx
+++ src/pages/VipTasks.jsx
@@ -3,71 +3,62 @@ import logo from "../assets/logo.jpg"
 
 const VipTasks = () => {
     return (  
-        <div className="min-h-screen flex justify-center items-center">
+        <div className="min-h-screen flex justify-center items-start">
             <div className="md:max-w-screen-md w-full">
-                <div className="flex fon p-3 rounded-xl m-2 justify-around items-start sm:justify-start sm:gap-8">
+                <div className="flex fon p-3 rounded-xl m-2 justify-around items-center sm:justify-start sm:gap-8">
                     <img className="w-20 rounded-full" src={logo} alt="logo" />
-                    <div className="flex flex-col items-start">
-                        <h1 className="text-2xl font-semibold">* * 1234</h1>
-                        <h3 className="text-lg font-semibold">17.04.2023</h3>
+                    <div className="flex flex-col items-start sm:w-full">
+                        <h1 className="text-3xl font-semibold">* * 1234</h1>
+                        <h3 className="text-xl font-semibold">17.04.2023</h3>
                     </div>
-                    <h1 className="text-4xl font-semibold italic sm:flex sm:justify-end sm:w-full">VIP5</h1>
+                    <h1 className="text-4xl font-bold italic sm:flex sm:justify-end sm:w-full">VIP5</h1>
                 </div>
-                <div className="max-h-f overflow-y-auto">
-                    <div className="fon p-3 m-2 rounded-xl flex justify-between items-center">
-                        <div className="flex flex-col items-start">
-                            <h2 className="italic font-semibold text-xl text-white">VIP1</h2>
-                            <p className="text-white">Mavjud vazifalar soni <span className="text-rose-500">1</span></p>
+                <div className="max-h-96 overflow-y-auto">
+                    <Link to="/task">
+                        <div className="fon p-3 m-2 rounded-xl flex justify-between items-center">
+                            <div className="flex flex-col items-start">
+                                <h2 className="italic font-semibold text-3xl text-white">VIP1</h2>
+                                <p className="text-white text-lg font-semibold">Mavjud vazifalar soni <span className="text-rose-500">1</span></p>
+                            </div>
+                                <i className="hover:bg-zinc-800 transition-all fa-solid fa-angle-right w-8 rounded-full h-8 flex justify-center items-center bg-zinc-900 text-yellow-500"></i>
                         </div>
-                        <Link to="/">
-                            <i className="hover:bg-zinc-800 transition-all fa-solid fa-angle-right w-8 rounded-full h-8 flex justify-center items-center bg-zinc-900 text-yellow-500"></i>
-                        </Link>
-                    </div>
-                    <div className="fon p-3 m-2 rounded-xl flex justify-between items-center">
-                        <div className="flex flex-col items-start">
-                            <h2 className="italic font-semibold text-xl text-white">VIP1</h2>
-                            <p className="text-white">Mavjud vazifalar soni <span className="text-rose-500">1</span></p>
+                    </Link>
+                    <Link to="/task">
+                        <div className="fon p-3 m-2 rounded-xl flex justify-between items-center">
+                            <div className="flex flex-col items-start">
+                                <h2 className="italic font-semibold text-3xl text-white">VIP1</h2>
+                                <p className="text-white text-lg font-semibold">Mavjud vazifalar soni <span className="text-rose-500">1</span></p>
+                            </div>
+                                <i className="hover:bg-zinc-800 transition-all fa-solid fa-angle-right w-8 rounded-full h-8 flex justify-center items-center bg-zinc-900 text-yellow-500"></i>
                         </div>
-                        <Link to="/">
-                            <i className="hover:bg-zinc-800 transition-all fa-solid fa-angle-right w-8 rounded-full h-8 flex justify-center items-center bg-zinc-900 text-yellow-500"></i>
-                        </Link>
-                    </div>
-                    <div className="fon p-3 m-2 rounded-xl flex justify-between items-center">
-                        <div className="flex flex-col items-start">
-                            <h2 className="italic font-semibold text-xl text-white">VIP1</h2>
-                            <p className="text-white">Mavjud vazifalar soni <span className="text-rose-500">1</span></p>
+                    </Link>
+                    <Link to="/task">
+                        <div className="fon p-3 m-2 rounded-xl flex justify-between items-center">
+                            <div className="flex flex-col items-start">
+                                <h2 className="italic font-semibold text-3xl text-white">VIP1</h2>
+                                <p className="text-white text-lg font-semibold">Mavjud vazifalar soni <span className="text-rose-500">1</span></p>
+                            </div>
+                                <i className="hover:bg-zinc-800 transition-all fa-solid fa-angle-right w-8 rounded-full h-8 flex justify-center items-center bg-zinc-900 text-yellow-500"></i>
                         </div>
-                        <Link to="/">
-                            <i className="hover:bg-zinc-800 transition-all fa-solid fa-angle-right w-8 rounded-full h-8 flex justify-center items-center bg-zinc-900 text-yellow-500"></i>
-                        </Link>
-                    </div>
-                    <div className="fon p-3 m-2 rounded-xl flex justify-between items-center">
-                        <div className="flex flex-col items-start">
-                            <h2 className="italic font-semibold text-xl text-white">VIP1</h2>
-                            <p className="text-white">Mavjud vazifalar soni <span className="text-rose-500">1</span></p>
+                    </Link>
+                    <Link to="/task">
+                        <div className="fon p-3 m-2 rounded-xl flex justify-between items-center">
+                            <div className="flex flex-col items-start">
+                                <h2 className="italic font-semibold text-3xl text-white">VIP1</h2>
+                                <p className="text-white text-lg font-semibold">Mavjud vazifalar soni <span className="text-rose-500">1</span></p>
+                            </div>
+                                <i className="hover:bg-zinc-800 transition-all fa-solid fa-angle-right w-8 rounded-full h-8 flex justify-center items-center bg-zinc-900 text-yellow-500"></i>
                         </div>
-                        <Link to="/">
-                            <i className="hover:bg-zinc-800 transition-all fa-solid fa-angle-right w-8 rounded-full h-8 flex justify-center items-center bg-zinc-900 text-yellow-500"></i>
-                        </Link>
-                    </div>
-                    <div className="fon p-3 m-2 rounded-xl flex justify-between items-center">
-                        <div className="flex flex-col items-start">
-                            <h2 className="italic font-semibold text-xl text-white">VIP1</h2>
-                            <p className="text-white">Mavjud vazifalar soni <span className="text-rose-500">1</span></p>
+                    </Link>
+                    <Link to="/task">
+                        <div className="fon p-3 m-2 rounded-xl flex justify-between items-center">
+                            <div className="flex flex-col items-start">
+                                <h2 className="italic font-semibold text-3xl text-white">VIP1</h2>
+                                <p className="text-white text-lg font-semibold">Mavjud vazifalar soni <span className="text-rose-500">1</span></p>
+                            </div>
+                                <i className="hover:bg-zinc-800 transition-all fa-solid fa-angle-right w-8 rounded-full h-8 flex justify-center items-center bg-zinc-900 text-yellow-500"></i>
                         </div>
-                        <Link to="/">
-                            <i className="hover:bg-zinc-800 transition-all fa-solid fa-angle-right w-8 rounded-full h-8 flex justify-center items-center bg-zinc-900 text-yellow-500"></i>
-                        </Link>
-                    </div>
-                    <div className="fon p-3 m-2 rounded-xl flex justify-between items-center">
-                        <div className="flex flex-col items-start">
-                            <h2 className="italic font-semibold text-xl text-white">VIP1</h2>
-                            <p className="text-white">Mavjud vazifalar soni <span className="text-rose-500">1</span></p>
-                        </div>
-                        <Link to="/">
-                            <i className="hover:bg-zinc-800 transition-all fa-solid fa-angle-right w-8 rounded-full h-8 flex justify-center items-center bg-zinc-900 text-yellow-500"></i>
-                        </Link>
-                    </div>
+                    </Link>
                 </div>
             </div>
         </div>
