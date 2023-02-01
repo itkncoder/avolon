b521ee3212db20b03187f7fa0647601b935701a2
KNCoder <itkncoder@gmail.com>
2023-02-01 23:21:03 +0500

    fixed bug with background image

--- src/pages/Referal.jsx
+++ src/pages/Referal.jsx
@@ -2,7 +2,7 @@ import {Link} from "react-router-dom"
 
 const Referal = () => {
     return (
-        <div className="max-h-screen">
+        <div className="min-h-screen">
             <div>
                 <Link to="/">
                     <i className="hover:bg-zinc-800 transition-all fa-solid fa-angle-left absolute top-2 left-2 w-8 rounded-full h-8 flex justify-center items-center bg-zinc-900 text-yellow-500"></i>
